1bb4e705530c5a910b090f7248575942cf74ae64
hanzlmichael <71646277+hanzlmichael@users.noreply.github.com>
2020-11-29 09:46:29 +0100

    Update OS208HanzlMichael.cpp

--- OS2/08/OS208HanzlMichael.cpp
+++ OS2/08/OS208HanzlMichael.cpp
@@ -26,7 +26,7 @@ void *generateTime(void* p) {
 
 		time(&t);
 		pthread_mutex_unlock(&mtx);
-		usleep(50);
+		usleep(50000);
 	}
 	pthread_exit(0);
 }
@@ -39,7 +39,7 @@ void *printTime(void* p) {
 			if (lastTime != 0 && !compareTime()) {
 				tm * tm_local = localtime(&t);
 				tm_local->tm_hour += 1;
-				strftime(buffer, 80, "%H:%M:%S", tm_local);
+				strftime(buffer, 20, "%H:%M:%S", tm_local);
 				cout << buffer << endl;
 				lastTime = t;
 				counter++;
@@ -60,7 +60,7 @@ void *printTime(void* p) {
 		}
 
 		pthread_mutex_unlock(&mtx);
-		usleep(50);
+		usleep(50000);
 	}
 	pthread_exit(0);
 }
